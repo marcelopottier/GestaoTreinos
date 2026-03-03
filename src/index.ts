ee619533539d56b94a93311706322c6bbf7a3e3a
Marcelo Pottier <marcelopottier@gmail.com>
2026-03-03 20:48:54 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen({ port: Number(process.env.PORT) || 8081 }, function (err) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
